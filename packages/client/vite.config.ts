a96784f4e95933bd71c76c7c4536731dc38fc3dc
Pedro <30800044+pricci1@users.noreply.github.com>
2026-02-07 12:06:44 -0300

    chore(client): add tailwindcss

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [tailwindcss(), react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
 
