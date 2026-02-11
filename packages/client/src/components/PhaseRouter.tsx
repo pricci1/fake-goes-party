9dd20c2419e2751631a7ef89f30607d3feb7a30a
Pedro <30800044+pricci1@users.noreply.github.com>
2026-02-11 18:27:44 -0300

    feat(client): default to spectator mode when game in progress

--- packages/client/src/components/PhaseRouter.tsx
+++ packages/client/src/components/PhaseRouter.tsx
@@ -1,6 +1,7 @@
-import { useAtomValue } from "jotai";
-import { currentPhaseAtom } from "../atoms";
-import { isSpectatorAtom } from "../atoms/modeAtoms";
+import { useEffect } from "react";
+import { useAtomValue, useSetAtom } from "jotai";
+import { currentPhaseAtom, myPlayerIndicesAtom } from "../atoms";
+import { gameModeAtom, isSpectatorAtom } from "../atoms/modeAtoms";
 import { Lobby } from "./Lobby";
 import { CategorySelection } from "./CategorySelection";
 import { CardReveal } from "./CardReveal";
@@ -24,6 +25,15 @@ function Loading() {
 export function PhaseRouter() {
   const phase = useAtomValue(currentPhaseAtom);
   const isSpectator = useAtomValue(isSpectatorAtom);
+  const myIndices = useAtomValue(myPlayerIndicesAtom);
+  const mode = useAtomValue(gameModeAtom);
+  const setSpectator = useSetAtom(isSpectatorAtom);
+
+  useEffect(() => {
+    if (mode === "remote" && myIndices.length === 0 && phase && phase !== "lobby") {
+      setSpectator(true);
+    }
+  }, [mode, myIndices.length, phase, setSpectator]);
 
   if (isSpectator) {
     return <SpectatorRouter phase={phase} />;
